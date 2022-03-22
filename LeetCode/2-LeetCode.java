9146dd2402b747f8a5bbfdef6d796db17c3886ed
Kumar Laxmikant <laxmikantk2002@gmail.com>
2022-03-22 09:11:21 +0530

    Create 2-LeetCode.java

---
+++ LeetCode/2-LeetCode.java
@@ -0,0 +1,24 @@
+//2. Palindrome Number
+//Difficulty: Easy
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        int r,sum=0,temp;
+        if(x<0)
+            return false;
+        else
+        {
+            temp=x;
+            while(x>0)    
+            {    
+                r=x%10;    
+                sum=(sum*10)+r;    
+                x=x/10;    
+            }
+            if(temp==sum)
+                return true;
+            else
+                return false;
+        }
+    }
+}
