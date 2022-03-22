20b098be8c96c77d426e10bf09724db6031a7cb4
Kumar Laxmikant <laxmikantk2002@gmail.com>
2022-03-22 09:28:42 +0530

    Create 69-LeetCode.java

---
+++ LeetCode/69-LeetCode.java
@@ -0,0 +1,10 @@
+// 69. Sqrt(x)
+// Difficulty: Easy
+
+class Solution {
+    public int mySqrt(int x) {
+        double ans1 = Math.sqrt(x);
+        int ans2 = (int)ans1;
+        return ans2;
+    }
+}
