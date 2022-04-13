8ce413efee23e440d5ef52f2fcc5286ceccbee23
Kumar Laxmikant <laxmikantk2002@gmail.com>
2022-04-13 12:09:18 +0530

    Create 26-LeetCode.cpp

---
+++ LeetCode/26-LeetCode.cpp
@@ -0,0 +1,16 @@
+// 26. Remove Duplicates from Sorted Array
+// Difficulty: Easy
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return 1
+        i = 0
+        while i<len(nums):
+            if i==len(nums)-1:
+                break
+            elif nums[i]==nums[i+1]:
+                nums.pop(i+1)
+            else:
+                i = i + 1
+        return len(nums)
