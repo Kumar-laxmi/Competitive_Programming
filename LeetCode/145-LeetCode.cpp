ad28e9781e6ab5e3afe5dc0bba73e2821103ffb2
Kumar Laxmikant <laxmikantk2002@gmail.com>
2022-04-16 15:21:22 +0530

    Create 145-LeetCode.cpp

---
+++ LeetCode/145-LeetCode.cpp
@@ -0,0 +1,27 @@
+// 145. Binary Tree Postorder Traversal
+// Difficulty: Easy
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> result;
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root) {
+            postorderTraversal(root->left);
+            postorderTraversal(root->right);
+            result.push_back(root->val);
+        }
+        
+        return result;
+    }
+};
