ecefd41d23c6852f8d04e0e6837916073c8f7fe6
Kumar Laxmikant <laxmikantk2002@gmail.com>
2022-07-31 15:35:12 +0530

    Create 704-LeetCode.java

---
+++ LeetCode/704-LeetCode.java
@@ -0,0 +1,24 @@
+// 704. Binary Search
+// Difficulty: Easy
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        int mid;
+        
+        while(low<=high) {
+            mid = ((low + high)/2);
+            if(nums[mid]==target) {
+                return mid;
+            }
+            else if(nums[mid]<target) {
+                low = mid + 1;
+            }
+            else if(nums[mid]>target) {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
