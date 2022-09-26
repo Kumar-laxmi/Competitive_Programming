bea4658afd6d33a0c43b326cb250d0126386879e
Kumar Laxmikant <laxmikantk2002@gmail.com>
2022-09-26 20:44:56 +0530

    Create Tom_and_Jerry.java

---
+++ Geeksforgeeks/Problem-Of-The-Day/Tom_and_Jerry.java
@@ -0,0 +1,12 @@
+//User function Template for Java
+
+class Solution {
+    static int numsGame(int N) {
+        if(N % 2 == 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+};
