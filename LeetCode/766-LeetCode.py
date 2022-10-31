c0d873d687782d02b650b1ccf73f9fae5851b8c9
Kumar Laxmikant <laxmikantk2002@gmail.com>
2022-10-31 16:53:59 +0530

    Create 766-LeetCode.py

---
+++ LeetCode/766-LeetCode.py
@@ -0,0 +1,10 @@
+# 766. Toeplitz Matrix
+# Difficulty: Easy
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(len(matrix)-1):
+            for j in range(len(matrix[0])-1):
+                if matrix[i][j] != matrix[i+1][j+1]:
+                    return False
+        return True
