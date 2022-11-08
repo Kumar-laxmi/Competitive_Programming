e42dfb2862a7a7e6862799c84721b31b7cc90c04
Kumar Laxmikant <laxmikantk2002@gmail.com>
2022-11-08 10:05:53 +0530

    Create 1544-LeetCode.cpp

---
+++ LeetCode/1544-LeetCode.cpp
@@ -0,0 +1,32 @@
+// 1544. Make The String Great
+// Difficulty: 
+
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char> st;
+        string result = "";
+
+        for(int i=0; i<s.length(); i++) {
+            if(st.empty()) {
+                st.push(s[i]);
+            }
+            else {
+                if(st.top() - s[i] == 32 || s[i] - st.top() == 32) {
+                    st.pop();
+                }
+                else {
+                    st.push(s[i]);
+                }
+            }
+        }
+
+        while(!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
